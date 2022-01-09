633a463eaf630c78fa361bcbd1302b6416ad67cb
clintonMF <mekwunyeclinton22@gmail.com>
2022-01-09 09:47:58 +0100

    tired

--- todolistapp/settings.py
+++ todolistapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from environs import Env
 env = Env() 
-env.read_env
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +82,9 @@ WSGI_APPLICATION = 'todolistapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-"default": env.dj_db_url("DATABASE_URL")
+"default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
 }
 
 
